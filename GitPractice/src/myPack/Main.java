13792ef363c1a0d7ea228dcfdfaa4c1b907fe4f3
admin <admin>
2018-02-11 14:45:20 +0900

    add: お天気占い実装
    テスト

--- GitPractice/src/myPack/Main.java
+++ GitPractice/src/myPack/Main.java
@@ -1,10 +1,28 @@
 package myPack;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args){
 
 		System.out.println("Hello World");
+
+		System.out.println("明日の天気は");
+		int rnd = new Random().nextInt(3);
+		switch(rnd){
+			case 0:
+				System.out.println("晴れ");
+				break;
+			case 1:
+				System.out.println("くもり");
+				break;
+			case 2:
+				System.out.println("雨");
+			default:
+				break;
+		}
+
 
 	}
 
